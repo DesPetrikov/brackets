d7083268034fc4ec491488e92a2b39dbdde0a265
DesPetrikov <desik1603@gmail.com>
2021-12-19 19:35:47 +0300

    feat: add solution to brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const pairsArr = bracketsConfig.map(el => el[0] + el[1]);
+	const arrFromStr = str.split('')
+	for(let i = 0; i < arrFromStr.length - 1; i += 1) {
+		if(pairsArr.includes(arrFromStr[i] + arrFromStr[i + 1])){
+			arrFromStr.splice(i, 2);
+			i = -1;	
+		}
+	}
+	return !arrFromStr.length
+	
 }
